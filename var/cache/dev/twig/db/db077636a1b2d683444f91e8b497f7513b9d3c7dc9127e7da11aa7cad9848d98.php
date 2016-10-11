0c5ab92a7eb124586ae61ef38aa7dead50de8b89
nirae <ndubouilh@gmail.com>
2016-10-11 16:40:31 +0200

    Install Symfony

---
+++ var/cache/dev/twig/db/db077636a1b2d683444f91e8b497f7513b9d3c7dc9127e7da11aa7cad9848d98.php
@@ -0,0 +1,48 @@
+<?php
+
+/* @Security/Collector/icon.svg */
+class __TwigTemplate_2586eb88b3525cb6029156aec20ee5b2db95f341f4be1828862aeb00a7ceeb5e extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_d32a9fc127d4bfe8a1ead45a8113d7f87e4e0c9065ae68ebd4a644c93c1b2d6a = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_d32a9fc127d4bfe8a1ead45a8113d7f87e4e0c9065ae68ebd4a644c93c1b2d6a->enter($__internal_d32a9fc127d4bfe8a1ead45a8113d7f87e4e0c9065ae68ebd4a644c93c1b2d6a_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "@Security/Collector/icon.svg"));
+
+        // line 1
+        echo "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" x=\"0px\" y=\"0px\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" enable-background=\"new 0 0 24 24\" xml:space=\"preserve\">
+    <path fill=\"#AAAAAA\" d=\"M21,20.4V22H3v-1.6c0-3.7,2.4-6.9,5.8-8c-1.7-1.1-2.9-3-2.9-5.2c0-3.4,2.7-6.1,6.1-6.1s6.1,2.7,6.1,6.1c0,2.2-1.2,4.1-2.9,5.2C18.6,13.5,21,16.7,21,20.4z\"/>
+</svg>
+";
+        
+        $__internal_d32a9fc127d4bfe8a1ead45a8113d7f87e4e0c9065ae68ebd4a644c93c1b2d6a->leave($__internal_d32a9fc127d4bfe8a1ead45a8113d7f87e4e0c9065ae68ebd4a644c93c1b2d6a_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@Security/Collector/icon.svg";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  22 => 1,);
+    }
+
+    public function getSource()
+    {
+        return "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" x=\"0px\" y=\"0px\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" enable-background=\"new 0 0 24 24\" xml:space=\"preserve\">
+    <path fill=\"#AAAAAA\" d=\"M21,20.4V22H3v-1.6c0-3.7,2.4-6.9,5.8-8c-1.7-1.1-2.9-3-2.9-5.2c0-3.4,2.7-6.1,6.1-6.1s6.1,2.7,6.1,6.1c0,2.2-1.2,4.1-2.9,5.2C18.6,13.5,21,16.7,21,20.4z\"/>
+</svg>
+";
+    }
+}
